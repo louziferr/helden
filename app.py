92ee2ed4d2bb3d524773659c55de0dd7aa998d1a
luzieahrens <luzie.ahrens@gmail.com>
2025-06-25 12:23:36 +0200

    added route held

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 # App erstellen
@@ -11,6 +11,24 @@ with open('static/helden.json') as f:
 @app.route('/')
 def index():
     return render_template('index.html', helden=helden)
+
+@app.route('/held')
+def held():
+    id = request.args.get('id')
+
+    if id: id = int(id)
+    else: return "No ID"
+
+    # Helden mit der ID raussuchen
+    gefunden = False
+    for held in helden:
+        if id == held['id']:
+            gefunden = True
+            break
+
+    if gefunden:
+        return render_template('held.html', held=held)
+    return 'Not Found'
 
 # App starten
 app.run(debug=True)
